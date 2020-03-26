d935866c85797e01689d2c9725542a3c65b7044c
devleonardo <leonardomartins.ads@gmail.com>
2020-03-26 00:45:29 -0300

    adição de dados

--- Project2/Origem.cpp
+++ Project2/Origem.cpp
@@ -2,19 +2,28 @@
 #include<stdio.h>
 #include<string.h>
 
+#define TAM 50
+#define RU 10
+
 struct Alunos {
 	int ru;
-	char nome[50];
+	char nome[TAM];
 	struct Alunos* direita, * esquerda;
+};
+struct Alunos all[RU] = {
+	{1,"Leonardo"},
+	{2,"Layla"},
+	{3,"Edson"}
 };
 
 int menu();
-void Inserir(Alunos** ElementoVarredura, int num, char nomes[50]);
-Alunos* Buscar(Alunos** ElementoVarredura, int num, char nomes[50]);
+void Inserir(Alunos** ElementoVarredura, int rus, char nomes[TAM]);
+Alunos* Buscar(Alunos** ElementoVarredura, int rus, char nomes[TAM]);
 
 int main() {
-	int op, c, num;
-	char nomes[50];
+	int op, c;
+	int rus;
+	char nomes[TAM];
 
 	Alunos* root;
 	root = (Alunos*)malloc(sizeof(Alunos));
@@ -28,20 +37,21 @@ int main() {
 		switch (op) {
 		case 1:
 			printf("Digite o RU do aluno: ");
-			scanf_s("%d", &num);
+			scanf_s("%d", &rus);
 			while ((c = getchar()) != '\n' && c != EOF) {} // sempre limpe o buffer do teclado.
 			printf("Digite o nome do aluno: ");
 			fgets(nomes, 50, stdin);
-			Inserir(&root, num, nomes);
+			Inserir(&root, rus, nomes);
 			break;
 		case 2:
 			printf("Digite o RU a ser buscado: ");
-			scanf_s("%d", &num);
+			scanf_s("%d", &rus);
 			while ((c = getchar()) != '\n' && c != EOF) {} // sempre limpe o buffer do teclado.
-			ElementoBusca = Buscar(&root, num, nomes);
+			ElementoBusca = Buscar(&root, rus, nomes);
 			if (ElementoBusca != 0) {
-				printf("%d\n", num);
-				printf("%s\n", nomes);
+				printf("\nRegistro encontrado!\n");
+				printf("  \nRU: %d\n", rus);
+				printf("Nome: %s\n", nomes);
 			}
 			else
 				printf("Registro nao encontrado!\n");
@@ -72,7 +82,7 @@ int menu() {
 	return op;
 }
 
-void Inserir(Alunos** ElementoVarredura, int num, char nomes[50]) {
+void Inserir(Alunos** ElementoVarredura, int rus, char nomes[TAM]) {
 
 	if (*ElementoVarredura == NULL)
 	{ //SE ESTÁ VAZIO, COLOCA NA ÁRVORE
@@ -81,44 +91,44 @@ void Inserir(Alunos** ElementoVarredura, int num, char nomes[50]) {
 		NovoElemento->esquerda = NULL;
 		NovoElemento->direita = NULL;
 
-		NovoElemento->ru = num;
-		NovoElemento->nome[50] = nomes[50];
+		NovoElemento->ru = rus;
+		NovoElemento->nome[TAM] = nomes[TAM];
 		*ElementoVarredura = NovoElemento;
 
 		return;
 	}
 
-	if (num < (*ElementoVarredura)->ru)
+	if (rus < (*ElementoVarredura)->ru)
 	{
-		Inserir(&(*ElementoVarredura)->esquerda, num, nomes);
+		Inserir(&(*ElementoVarredura)->esquerda, rus, nomes);
 	}
 	else
 	{
-		if (num > (*ElementoVarredura)->ru)
+		if (rus > (*ElementoVarredura)->ru)
 		{
-			Inserir(&(*ElementoVarredura)->direita, num, nomes);
+			Inserir(&(*ElementoVarredura)->direita, rus, nomes);
 		}
 	}
 }
 
-Alunos* Buscar(Alunos** ElementoVarredura, int num, char nomes[50])
+Alunos* Buscar(Alunos** ElementoVarredura, int rus, char nomes[TAM])
 {
 	if (*ElementoVarredura == NULL)
 		return NULL;
 
-	if (num < (*ElementoVarredura)->ru)
+	if (rus < (*ElementoVarredura)->ru)
 	{
-		Buscar(&((*ElementoVarredura)->esquerda), num, nomes);
+		Buscar(&((*ElementoVarredura)->esquerda), rus, nomes);
 	}
 	else
 	{
-		if (num > (*ElementoVarredura)->ru)
+		if (rus > (*ElementoVarredura)->ru)
 		{
-			Buscar(&((*ElementoVarredura)->direita), num, nomes);
+			Buscar(&((*ElementoVarredura)->direita), rus, nomes);
 		}
 		else
 		{
-			if (num == (*ElementoVarredura)->ru)
+			if (rus == (*ElementoVarredura)->ru)
 				return *ElementoVarredura;
 		}
 	}
